5c8b661f5165bbd9b2e6ccebbed33bbb43f7db1f
Thomas Mailund <mailund@birc.au.dk>
2021-12-17 13:39:37 +0100

    SOLUTIONS! Don't give them to the students, but they are nice to have when writing the tests

--- src/align.py
+++ src/align.py
@@ -2,12 +2,102 @@
 
 
 def align(x: str, y: str, edits: str) -> tuple[str, str]:
-    return ("", "")
+    """Align two sequences from a sequence of edits.
+
+    Args:
+        x (str): The first sequence to align.
+        y (str): The second sequence to align
+        edits (str): The list of edits to apply, given as a string
+
+    Returns:
+        tuple[str, str]: The two rows in the pairwise alignment
+
+    >>> align("ACCACAGTCATA", "ACAGAGTACAAA", "MDMMMMMMIMMMM")
+    ('ACCACAGT-CATA', 'A-CAGAGTACAAA')
+
+    """
+    i, j = 0, 0
+    row_x, row_y = [], []
+    for op in edits:
+        match op:
+            case 'M':
+                row_x.append(x[i])
+                row_y.append(y[j])
+                i += 1
+                j += 1
+            case 'D':
+                row_x.append(x[i])
+                row_y.append('-')
+                i += 1
+            case 'I':
+                row_x.append('-')
+                row_y.append(y[j])
+                j += 1
+
+    return "".join(row_x), "".join(row_y)
 
 
 def edits(x: str, y: str) -> str:
-    return ""
+    """Extract the edit operations from a pairwise alignment.
+
+    Args:
+        x (str): The first row in the pairwise alignment.
+        y (str): The second row in the pairwise alignment.
+
+    Returns:
+        str: The list of edit operations as a string.
+
+    >>> edits('ACCACAGT-CATA', 'A-CAGAGTACAAA')
+    'MDMMMMMMIMMMM'
+
+    """
+    assert len(x) == len(y)
+    edits = [None] * len(x)
+    for i, _ in enumerate(x):
+        if x[i] == '-':
+            edits[i] = 'I'
+        elif y[i] == '-':
+            edits[i] = 'D'
+        else:
+            edits[i] = 'M'
+    return "".join(edits)
 
 
-def local_align(chrom: str, pos: int, pat: str, edits: str) -> tuple[str, str]:
-    return ("", "")
+def local_align(chrom: str, pos: int, read: str, edits: str) -> tuple[str, str]:
+    """Extracts a local alignment from a chromosome and a pattern.
+
+    This function works almost as align(), except that it doesn't
+    align all of the chrom sequence but only a subsequence of it.
+
+    Args:
+        chrom (str): A (potentially large) chromosomal sequence.
+        pos (int): The position in the chromosomal sequence, 0-indexed, where
+                   the alignment starts.
+        read (str): The read we have matched against the genome.
+        edits (str): The sequence of edit operations.
+
+    Returns:
+        tuple[str, str]: The two rows in the local pairwise alignment
+
+    >>> local_align("GTAACCACAGTCATA", 3, "ACAGAGTACAAA", "MDMMMMMMIMMMM")
+    ('ACCACAGT-CATA', 'A-CAGAGTACAAA')
+    """
+    i, j = pos, 0
+    row_x, row_y = [], []
+    for op in edits:
+        match op:
+            case 'M':
+                row_x.append(chrom[i])
+                row_y.append(read[j])
+                i += 1
+                j += 1
+            case 'D':
+                row_x.append(chrom[i])
+                row_y.append('-')
+                i += 1
+            case 'I':
+                row_x.append('-')
+                row_y.append(read[j])
+                j += 1
+
+    return "".join(row_x), "".join(row_y)
--- src/cigar.py
+++ src/cigar.py
@@ -1,19 +1,68 @@
 """A module for translating between alignments and edits sequences."""
 
+import re
+
 
 def split_pairs(cigar: str) -> list[tuple[int, str]]:
-    # FIXME: TM you have to implement this
-    return []
+    """Split a CIGAR string into a list of integer-operation pairs.
+
+    Args:
+        cigar (str): A CIGAR string
+
+    Returns:
+        list[tuple[int, str]]: A list of pairs, where the first element is
+        an integer and the second an edit operation.
+
+    >>> split_pairs("1M1D6M1I4M")
+    [(1, 'M'), (1, 'D'), (6, 'M'), (1, 'I'), (4, 'M')]
+
+    """
+    return [(int(i), op) for i, op in re.findall(r"(\d+)([^\d]+)", cigar)]
 
 
 def cigar_to_edits(cigar: str) -> str:
-    return ""
+    """Expand the compressed CIGAR encoding into the full list of edits.
+
+    Args:
+        cigar (str): A CIGAR string
+
+    Returns:
+        str: The edit operations the CIGAR string describes.
+
+    >>> cigar_to_edits("1M1D6M1I4M")
+    'MDMMMMMMIMMMM'
+
+    """
+    return "".join([op * i for i, op in split_pairs(cigar)])
 
 
 def split_blocks(x: str) -> list[str]:
-    # FIXME: TM you have to implement this
-    return []
+    """Split a string into blocks of equal character.
+
+    Args:
+        x (str): A string, but we sorta think it would be edits.
+
+    Returns:
+        list[str]: A list of blocks.
+
+    >>> split_blocks('MDMMMMMMIMMMM')
+    ['M', 'D', 'MMMMMM', 'I', 'MMMM']
+
+    """
+    return [m[0] for m in re.findall(r"((.)\2*)", x)]
 
 
 def edits_to_cigar(edits: str) -> str:
-    return ""
+    """Encode a sequence of edits as a CIGAR.
+
+    Args:
+        edits (str): A sequence of edit operations
+
+    Returns:
+        str: The CIGAR encoding of edits.
+
+    >>> edits_to_cigar('MDMMMMMMIMMMM')
+    '1M1D6M1I4M'
+
+    """
+    return "".join(f"{len(b)}{b[0]}" for b in split_blocks(edits))
