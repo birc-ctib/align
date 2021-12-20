cee040789f53297d3af08f2c0e6a7f965de4d523
Thomas Mailund <mailund@birc.au.dk>
2021-12-20 12:22:17 +0100

    pytests

--- src/test_align.py
+++ src/test_align.py
@@ -1,11 +1,26 @@
 """Tests for the align module."""
 
+from align import align, edits
+
 
 def test_align() -> None:
     """Testing the align() function."""
-    assert False
+    assert align(
+        "accaaagta",
+        "acaaatgtcca",
+        "MDMMIMMMMIIM"
+    ) == ("acca-aagt--a", "a-caaatgtcca")
+    assert align(
+        "a", "", "1D"
+    ) == ("a", "-")
 
 
 def test_edits() -> None:
     """Testing the edits() function."""
-    assert False
+    assert edits(
+        "acca-aagt--a", "a-caaatgtcca"
+    ) == "MDMMIMMMMIIM"
+    assert edits(
+        "acgttcga",
+        "aaa---aa"
+    ) == "MMMDDDMM"
