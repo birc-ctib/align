deb2442089a806bf816886c64976d6f2e504c29a
Thomas Mailund <mailund@birc.au.dk>
2022-12-13 13:54:23 +0100

    added an autograde test

---
+++ .test/scripts/two-files.sh
@@ -0,0 +1,9 @@
+out=$(mktemp /tmp/ctib.XXXXXX)
+function cleanup { rm $out ; }
+trap cleanup EXIT
+
+python3 src/main.py from_cig data/cigs.in $out
+.test/scripts/cmp.sh $out data/cigs.out || exit 1
+
+python3 src/main.py to_cig data/alignments.in $out
+.test/scripts/cmp.sh $out data/alignments.out || exit 1
